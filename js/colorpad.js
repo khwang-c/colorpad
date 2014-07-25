0d31e5f82b77ea5c3f2853a3b4f8a5e969eda606
Kevin Hwang <khwang.c@gmail.com>
2014-07-25 01:30:06 -0400

    more functions

--- js/colorpad.js
+++ js/colorpad.js
@@ -1,29 +1,63 @@
+var containerWidth = 960;
 
+function createGrid(size){
+	//set up grid
+		var boxsize = (containerWidth - (4*size))/size;
 
-
-//main
-$(document).ready(
-	function(){
-
-
-		//set up grid
-		for(var r = 1; r < 17; r++){
-			for(var j = 1; j < 17; j++){
+		for(var r = 0; r < size; r++){
+			for(var j = 0; j < size; j++){
 				//$('#container').append("<div class=\"row"+r+"\"></div>");
 				$('#container').append("<div class=\"cell\"></div>");
 			}
-			$('#container').append("</br>");
-			
+			$('#container').append("</br>");	
 		}
+		$('.cell').addClass("cell").height(boxsize).width(boxsize);
+};
 
-		$('.cell').hover(
-			function(){
+
+//main
+$(document).ready(function(){
+
+		createGrid(16);
+
+		
+
+		$('#clear').click(function(){
+			$('.cell').css("background-color","#BABFBC");
+		});
+
+		$('#reset').click(function(){
+			
+			var size = prompt("Please choose a size between 1 and 50.");
+
+			while(size < 1 || size > 50){
+				size = prompt("Invalid Size. Please choose a size between 1 and 50.");
+			}
+
+			$('#container').empty(); //empty removes all child nodes in the set of matched elements
+			createGrid(size);
+			//events();
+		});
+
+		$('mode').click(function(){
+
+		});
+
+		$('#container').on("mouseenter", ".cell" ,function(){
+			$(this).css("background-color","#4ab579");
+		}); //delegate binds event handlers to elements for current AND future elements in the DOM. 
+		//but it is superceded by .on method. .on does not work with event handler hover
+		
+		//hover will not work after grid has been reset because hover uses bind which binds event handlers
+		//to only current elements in the DOM.
+		/*
+		$('.cell').hover(function(){ 
 				$(this).css("background-color","#4ab579");
-		},
+		,
 			function(){
 				$(this).css("background-color","#BABFBC");
 			}
-		);
+		});*/
 
 
 });
